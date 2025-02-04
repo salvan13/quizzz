3b235363a1ba5daa219f7025fef4be7c9ee52857
Antonio Salvati <salvan13@gmail.com>
2025-02-04 23:06:16 +0100

    fix null username bug

--- public/player.js
+++ public/player.js
@@ -7,37 +7,41 @@ if (!pid) {
 }
 
 const username = prompt("Your Name:");
-if (!username) {
+if (username) {
+  start();
+} else {
   location.reload();
 }
 
-const socket = io({ query: { clientType, username, pid } });
+function start() {
+  const socket = io({ query: { clientType, username, pid } });
 
-socket.on("connect", () => {
-  console.log("socket connected");
-});
+  socket.on("connect", () => {
+    console.log("socket connected");
+  });
 
-socket.on("state", (data) => {
-  data = JSON.parse(data);
-  console.log("state", data);
-  document.querySelector("main").dataset.status = data.status;
+  socket.on("state", (data) => {
+    data = JSON.parse(data);
+    console.log("state", data);
+    document.querySelector("main").dataset.status = data.status;
 
-  const messageEl = document.querySelector(".message");
-  if (data.status === "waiting") {
-    messageEl.innerHTML = "Waiting for Quiz start...";
-  } else if (data.status === "over") {
-    messageEl.innerHTML = "Quiz Over";
-  } else {
-    messageEl.innerHTML = "";
-  }
+    const messageEl = document.querySelector(".message");
+    if (data.status === "waiting") {
+      messageEl.innerHTML = "Waiting for Quiz start...";
+    } else if (data.status === "over") {
+      messageEl.innerHTML = "Quiz Over";
+    } else {
+      messageEl.innerHTML = "";
+    }
 
-  document.querySelector(".buttons").dataset.done = false;
-  document.querySelector(".buttons").innerHTML = data.currentQuestion?.responses.map((resp, i) => `<button data-index="${i}"><b>${i}</b></button>`).join("") || "<div class=\"loader\"><b>.</b><b>.</b><b>.</b></div>";
-  Array.from(document.querySelectorAll(".buttons button")).forEach(button => {
-    button.addEventListener("click", () => {
-      socket.emit("answer", { index: parseInt(button.dataset.index) });
-      button.dataset.clicked = true;
-      document.querySelector(".buttons").dataset.done = true;
+    document.querySelector(".buttons").dataset.done = false;
+    document.querySelector(".buttons").innerHTML = data.currentQuestion?.responses.map((resp, i) => `<button data-index="${i}"><b>${i}</b></button>`).join("") || "<div class=\"loader\"><b>.</b><b>.</b><b>.</b></div>";
+    Array.from(document.querySelectorAll(".buttons button")).forEach(button => {
+      button.addEventListener("click", () => {
+        socket.emit("answer", { index: parseInt(button.dataset.index) });
+        button.dataset.clicked = true;
+        document.querySelector(".buttons").dataset.done = true;
+      });
     });
   });
-});
+};
